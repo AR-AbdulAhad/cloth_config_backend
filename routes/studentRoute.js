8ea78b8f632ce6b62d740491560745be619de5d6
sarwarsamad27 <yamanaamir2004@gmail.com>
2026-03-26 13:55:02 +0500

    added changes

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -14,7 +14,7 @@ router.get("/my-order-history", studentAuth, getMyOrderHistory);
 router.delete("/history/:id", studentAuth, deleteHistory);
 // router.post("/back-designs", studentAuth, listMyBackDesigns);
 router.post("/back-designs", studentAuth, getMyClassBackDesign);
-// router.post("/place-order", studentAuth, placeOrder);
+router.post("/place-order", studentAuth, placeOrder);
 router.post("/logos", studentAuth, listSchoolLogos);
 router.post("/class-back-designs", studentAuth, listBackDesigns);
 router.get("/configurator-back-design", studentAuth, getConfiguratorBackDesign);
