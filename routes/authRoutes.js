e19b6518eafa3f04a5c59ea7139eae4d3cfdc76d
Yaman Aamir <136349726+YamanAamir@users.noreply.github.com>
2026-03-30 20:03:39 +0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,5 +16,5 @@ router.get("/decode-registration-token", decodeRegistrationToken);
 router.post("/set-password", setUserPassword);
 // GET /api/auth/me/menus - Get dynamic sidebar menus based on role
 router.get("/sidebar-menus", authMiddleware(), getSidebarMenus);
-
+ 
 export default router;
