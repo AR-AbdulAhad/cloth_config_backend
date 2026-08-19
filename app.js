33d069dbef50daea40842a0d26a230187a76e1b0
yamanaamir <yamanaamir2004@gmail.com>
2026-08-19 20:04:27 +0500

    added

--- app.js
+++ app.js
@@ -46,10 +46,29 @@ io.on("connection", (socket) => {
     });
 });
 
+// Explicit CORS headers and preflight middleware
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (origin) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    } else {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+    }
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+    next();
+});
+
 app.use(cors({
-    origin: "*",
+    origin: (origin, callback) => callback(null, true),
+    credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"]
 }));
 
 // Need raw body for Stripe webhook signature verification
